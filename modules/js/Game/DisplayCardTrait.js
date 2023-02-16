d6fd25dfad555c9832ae2a0ff4edd12b3363566b
mykywar <mr.kywar@gmail.com>
2023-02-16 23:00:10 +0100

    [WIP][sc-241]Js is back !

--- modules/js/Game/DisplayCardTrait.js
+++ modules/js/Game/DisplayCardTrait.js
@@ -13,21 +13,32 @@ define([
 
                 constructor: function () {
                     this.debug('smilelife.DisplayCardTrait constructor');
-//                    this.handCards = [];
                 },
 
                 displayCards: function (gamedatas) {
-                    this.debug("DCT - DisplayCards", gamedatas);
+                    //this.debug("DCT - DisplayCards", gamedatas);
 
                     for (var cardId in gamedatas.myhand) {
                         this.debug(cardId, gamedatas.myhand[cardId]);
                         var card = gamedatas.myhand[cardId];
-                        
-                        dojo.place(this.format_block('jstpl_card', card), 'myhand');
+                        card.id = cardId;
+
+                        dojo.place(this.displayCard(card), 'myhand');
                     }
-
-
                 },
+
+                displayCard: function (card) {
+                    return `
+                        <div class="cardontable card_` + card.type + ` ` + card.shortclass + `" id="` + card.location + "_card_" + card.id + `" data-id="` + card.id + `">
+                            <span class="card_text card_title">` + card.title + `</span>
+                            <span class="card_text card_subtitle">` + card.subtitle + `</span>
+                            <span class="card_text card_text1">` + card.text1 + `</span>
+                            <span class="card_text card_text2">` + card.text2 + `</span>
+                            <span class="debug">` + card.type + `</span>
+                        </div>`;
+                }
+
+
             }
 
 
