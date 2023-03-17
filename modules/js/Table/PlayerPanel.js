c8c521b762f7ddf1f0b8ed9b6aea6e4f18f3b113
mykywar <mr.kywar@gmail.com>
2023-03-17 21:26:04 +0100

    [sc-323] prepare js

---
+++ modules/js/Table/PlayerPanel.js
@@ -0,0 +1,15 @@
+define([
+    "dojo",
+    "dojo/_base/declare",
+], function (dojo, declare) {
+    return declare(
+            "smilelife.playerpanel",
+            [],
+            {
+                constructor: function () {
+                    //this.debug("smilelife.playerpanel");
+                },
+
+            }
+    );
+});
