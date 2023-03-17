2b1b41b22af80167110a1da68e38523532f48c1e
mykywar <mr.kywar@gmail.com>
2023-03-17 12:33:39 +0100

    [sc-329] Infos Size Displayed

--- modules/js/Table/TableDraw.js
+++ modules/js/Table/TableDraw.js
@@ -30,40 +30,7 @@ define([
                 
                 
                 
-                
-                
-////                displayDeckAndDiscard: function (gamedatas) {
-////
-////
-////                    dojo.place(this.displayDeck(gamedatas), "deck_and_discard");
-////                    dojo.place(this.displayDiscard(gamedatas), "deck_and_discard");
-////                },
-//
-//                displayDeck: function (gamedatas) {
-//                    if (0 == gamedatas.deck) {
-//                        return `
-//                        <div class="cardontable empty_pile cards_stack" id="deck" data-id="0">
-//                            <div id="deck_counter" class="pile_counter">` + gamedatas.deck + `</div>
-//                        </div>`;
-//                    } else {
-//                        return `
-//                        <div class="cardontable card_0 card_deck cards_stack" id="card_0" data-id="0">
-//                            <div id="deck_counter" class="pile_counter">` + gamedatas.deck + `</div>
-//                        </div>`;
-//                    }
-//                },
-//
-//                displayDiscard: function (gamedatas) {
-//                    this.discard = gamedatas.discard;
-//                    if (null == gamedatas.discard) {
-//                        return `
-//                        <div class="cardontable empty_pile cards-stack" id="deck" data-id="0">
-//                            
-//                        </div>`;
-//                    } else {
-//                        return this.displayCard(gamedatas.discard);
-//                    }
-//                },
+               
             }
                     
                     
