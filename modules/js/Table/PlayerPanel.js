8757e95431284848791698fe2d70d7bc95c4699b
mykywar <mr.kywar@gmail.com>
2023-03-18 17:12:19 +0100

    [wip][sc-331] Display Panel info

--- modules/js/Table/PlayerPanel.js
+++ modules/js/Table/PlayerPanel.js
@@ -18,30 +18,25 @@ define([
                         this.debug("PA : "+playerId+" : ",player);
                         
                         dojo.place(this.getPlayerPanelHtml(player), "player_board_"+player.id);
+                        var handCounter = new ebg.counter();
+                        handCounter.create("player_hand_counter_".concat(player.id));
+                        handCounter.setValue(player.hand);
                         
+                        var maxhandCounter = new ebg.counter();
+                        maxhandCounter.create("player_maxhand_counter_".concat(player.id));
+                        maxhandCounter.setValue(player.attributes.maxCards);
+                        this.maxhandCounters[player.id] = maxhandCounter;
                     }
-                    
-                    
-                    
-//                     var playerId = Number(player.id);
-//            // show end game points
-//            dojo.place("<span class=\"end-game-points\">&nbsp;/&nbsp;".concat(POINTS_FOR_PLAYERS[Object.keys(gamedatas.players).length], "</span>"), "player_score_".concat(playerId), 'after');
-//            // hand cards counter
-//            dojo.place("<div class=\"counters\">\n                <div id=\"playerhand-counter-wrapper-".concat(player.id, "\" class=\"playerhand-counter\">\n                    <div class=\"player-hand-card\"></div> \n                    <span id=\"playerhand-counter-").concat(player.id, "\"></span>\n                </div>\n            </div>"), "player_board_".concat(player.id));
-//            var handCounter = new ebg.counter();
-//            handCounter.create("playerhand-counter-".concat(playerId));
-//            handCounter.setValue(player.handCards.length);
-//            _this.handCounters[playerId] = handCounter;
                 },
                 
                 
                 getPlayerPanelHtml: function (player) {
                     return `
                         <div class="counters" id="playerpanel_`+player.id+`">
-                            <div class="playerhand-counter">
-                                <span id="playerhand-counter-wrapper-`+player.id+`"></span>
+                            <div class="playerhand_counter">
+                                <span id="player_hand_counter_`+player.id+`"></span>
                                 /
-                                <span id="maxhand-counter-wrapper-`+player.id+`"></span>
+                                <span id="player_maxhand_counter_`+player.id+`"></span>
                             </div> 
                         </div>
                     `;
