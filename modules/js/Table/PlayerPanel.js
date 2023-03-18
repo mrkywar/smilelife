c47b57055e50519d6704ebc26f2e8d4ca10583e5
mykywar <mr.kywar@gmail.com>
2023-03-18 21:16:21 +0100

    [sc-331]Finished Player Panel (only card)

--- modules/js/Table/PlayerPanel.js
+++ modules/js/Table/PlayerPanel.js
@@ -7,44 +7,45 @@ define([
             [],
             {
                 constructor: function () {
+                    this.handCounters = [];
+                    this.maxhandCounters = [];
                 },
-                
+
                 displayPanels: function () {
                     var gamedatas = this.gamedatas;
-                    
+
                     for (var playerId in gamedatas.player) {
                         var player = gamedatas.player[playerId];
                         player.id = playerId;
-                        this.debug("PA : "+playerId+" : ",player);
-                        
-                        dojo.place(this.getPlayerPanelHtml(player), "player_board_"+player.id);
+
+                        dojo.place(this.getPlayerPanelHtml(player), "player_board_" + player.id);
                         var handCounter = new ebg.counter();
                         handCounter.create("player_hand_counter_".concat(player.id));
                         handCounter.setValue(player.hand);
-                        
+                        this.handCounters[playerId] = handCounter;
+
                         var maxhandCounter = new ebg.counter();
                         maxhandCounter.create("player_maxhand_counter_".concat(player.id));
                         maxhandCounter.setValue(player.attributes.maxCards);
-                        this.maxhandCounters[player.id] = maxhandCounter;
+                        this.maxhandCounters[playerId] = maxhandCounter;
                     }
                 },
-                
-                
+
                 getPlayerPanelHtml: function (player) {
                     return `
-                        <div class="counters" id="playerpanel_`+player.id+`">
+                        <div class="player_counters" id="playerpanel_` + player.id + `">
                             <div class="playerhand_counter">
-                                <span id="player_hand_counter_`+player.id+`"></span>
-                                /
-                                <span id="player_maxhand_counter_`+player.id+`"></span>
+                                <span id="player_hand_counter_` + player.id + `"></span>
+                                <span>/</span>
+                                <span id="player_maxhand_counter_` + player.id + `"></span>
                             </div> 
                         </div>
                     `;
-                    
-                    
+
+
                 }
-                
-                
+
+
 
             }
     );
