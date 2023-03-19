a6d5b09baa9e511b150a7352fda8ea23a47a7a0c
mykywar <mr.kywar@gmail.com>
2023-03-19 23:19:38 +0100

    [wip][sc-338] First Try for set first player

--- modules/php/Game/Game/GameInitializer.php
+++ modules/php/Game/Game/GameInitializer.php
@@ -2,7 +2,9 @@
 
 namespace SmileLife\Game\Game;
 
+use Core\DB\QueryString;
 use Core\Managers\PlayerManager;
+use SmileLife;
 use SmileLife\Game\Card\CardManager;
 use SmileLife\Game\PlayerAttributes\PlayerAttributesManager;
 use SmileLife\Game\Table\PlayerTableManager;
@@ -49,6 +51,12 @@ class GameInitializer {
      * @var ScoreComputer
      */
     private $scoreComputer;
+    
+    /**
+     * 
+     * @var SmileLife
+     */
+    private $game;
 
     public function __construct() {
         $this->gameManager = new GameManager();
@@ -57,6 +65,7 @@ class GameInitializer {
         $this->scoreComputer = new ScoreComputer();
         $this->playerTableManager = new PlayerTableManager();
         $this->playerAttributesManager = new PlayerAttributesManager();
+        $this->game = SmileLife::getInstance();
     }
 
     public function init($players, $options = array()) {
@@ -67,7 +76,19 @@ class GameInitializer {
         $this->playerTableManager->initNewGame();
         
         
-        var_dump($players);die;
+        //Define first Player
+        $oPlayers = $this->playerManager->findBy(null,null,[
+            "no" => QueryString::ORDER_ASC
+        ]);
+        $scores = [];
+        
+        foreach ($oPlayers as $player){
+            $cardsInHand = $this->cardManager->getPlayerCards($player);
+            $scores[$player->getId()] = $this->scoreComputer->compute($cardsInHand);
+        }
+        arsort($scores);
+        
+        $this->game->gamestate->changeActivePlayer(array_key_first($scores));
         
         
     }
