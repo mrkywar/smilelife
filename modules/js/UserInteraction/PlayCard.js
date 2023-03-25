52dc48db8247c4df1f19e942117eecfe87d24adb
mykywar <mr.kywar@gmail.com>
2023-03-25 13:33:14 +0100

    [sc-45]Selected Card in JS (no css associated)

--- modules/js/UserInteraction/PlayCard.js
+++ modules/js/UserInteraction/PlayCard.js
@@ -8,11 +8,32 @@ define([
             {
                 constructor: function () {
                 },
-                
-                
+
                 addPlayCardInteraction: function () {
                     this.addActionButton('play_button', _('Play card'), 'doPlay', null, false, 'blue');
                     this.addActionButton('discard_button', _('Discard card and pass'), 'doPass', null, false, 'red');
+
+                    var _this = this;
+
+                    dojo.query("#myhand .cardontable").addClass("selectable");
+                    dojo.query(".cardontable.selectable").connect('onclick', this, evt => {
+                        evt.preventDefault();
+                        evt.stopPropagation();
+
+                        _this.selectCard(evt.target);
+//                        evt.target.addClass("selected");
+
+                    });
+
+                },
+
+                selectCard: function (target) {
+                    if ('' !== target.id && $(target.id).classList.contains("selectable")) {
+                        this.debug(dojo.query(target.id), $(target.id));
+                        $(target.id).classList.add("selected");
+                    } else {
+                        this.selectCard(target.parentNode);
+                    }
                 },
 
                 doPlay: function () {
@@ -28,7 +49,7 @@ define([
                 },
 
                 doPass: function () {
-                    this.debug("DoPlay:");
+                    this.debug("DoPass:");
                 },
 
             }
