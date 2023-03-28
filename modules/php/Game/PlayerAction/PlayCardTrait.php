6c8987c5be6ca7e915d628fbe267e3eaf9c077a9
mykywar <mr.kywar@gmail.com>
2023-03-28 13:24:08 +0200

    [sc-45]Play with notif

--- modules/php/Game/PlayerAction/PlayCardTrait.php
+++ modules/php/Game/PlayerAction/PlayCardTrait.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace SmileLife\Game\PlayerAction;
 
+use Core\Models\Player;
+use SmileLife\Game\Card\Card;
 use SmileLife\Game\Card\Core\CardDecorator;
 use SmileLife\Game\Card\Core\CardSerializer;
+use SmileLife\Game\Table\PlayerTable;
 use SmileLife\Game\Table\PlayerTableDecorator;
 
 /**
@@ -13,12 +17,33 @@ trait PlayCardTrait {
 
     public function actionPlayCard($cardId) {
         $playerId = self::getCurrentPlayerId();
-        
+        $tableDecorator = new PlayerTableDecorator();
+        $cardDecorator = new CardDecorator(new CardSerializer());
+
         $card = $this->cardManager->findBy([
             "id" => $cardId
         ]);
-        var_dump($card,$cardId);die;
+        $player = $this->playerManager->findOne([
+            "id" => $playerId
+        ]);
+        $table = $this->tableManager->findOneBy([
+            "id" => $playerId
+        ]);
+
+        $this->cardManager->playCard($player, $card);
+
+        $table->addCard($card);
+        $this->tableManager->updateTable($table);
         
+        self::notifyAllPlayers('playCardNotification', clienttranslate('${player_name} play ${cardName}'), [
+            'playerId' => $playerId,
+            'player_name' => $player->getName(),
+            'cardName' => $card->getTitle(),
+            'table' => $tableDecorator->decorate($table),
+            'card' => $cardDecorator->decorate($card),
+        ]);
+        
+        $this->gamestate->nextState("playCard");
     }
 
 }
