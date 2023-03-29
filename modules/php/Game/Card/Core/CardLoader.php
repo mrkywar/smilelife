566d619b25df345537e8788cba3598014bb9f13e
mykywar <mr.kywar@gmail.com>
2023-03-29 21:22:45 +0200

    [sc-402] First Card Loader load Only Card in Category

--- modules/php/Game/Card/Core/CardLoader.php
+++ modules/php/Game/Card/Core/CardLoader.php
@@ -13,10 +13,11 @@ use RecursiveIteratorIterator;
 abstract class CardLoader {
 
     private const CARD_BASEPATH = "/Card";
+    private const CARD_CATEGORY_PATH = "/Category";
 
     static public function load() {
         $namespace = substr(dirname(__FILE__), 0, strrpos(dirname(__FILE__), self::CARD_BASEPATH) + strlen(self::CARD_BASEPATH));
-        $namespace .= "";
+        $namespace .= self::CARD_CATEGORY_PATH;
 
         foreach (self::getFilesList($namespace) as $file) {
             require_once ($file->getPathname());
@@ -26,7 +27,7 @@ abstract class CardLoader {
     static public function getFilesList($namespace) {
         $files = [];
 
-        $dir_iterator = new RecursiveDirectoryIterator(dirname($namespace));
+        $dir_iterator = new RecursiveDirectoryIterator($namespace);
         $iterator = new RecursiveIteratorIterator($dir_iterator);
         foreach ($iterator as $file) {
             if ($file->isDir()) {
