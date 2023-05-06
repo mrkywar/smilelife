b957f4c5f0745b77a6092c37a9534249c5366e65
mykywar <mr.kywar@gmail.com>
2023-05-06 14:52:46 +0200

    [sc-441] CriterionGroup

--- modules/php/Game/Card/Criterion/CriterionInterface.php
+++ modules/php/Game/Card/Criterion/CriterionInterface.php
@@ -1,20 +1,10 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-namespace SmileLife\Card\Criterion\CardCriterion;
+namespace SmileLife\Card\Criterion;
 
 /**
  *
  * @author Mr_Kywar mr_kywar@gmail.com
  */
 interface CriterionInterface {
-
     public function isValided(): bool;
-
-    public function isNotValided(): bool;
 }
