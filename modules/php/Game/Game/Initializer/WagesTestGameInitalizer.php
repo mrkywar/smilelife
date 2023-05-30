99ed06e531dc8c51f57ef4820ec344e97bd9e18d
mykywar <mr.kywar@gmail.com>
2023-05-30 13:59:16 +0200

    [sc-515] All Tests Wages implemented

--- modules/php/Game/Game/Initializer/WagesTestGameInitalizer.php
+++ modules/php/Game/Game/Initializer/WagesTestGameInitalizer.php
@@ -33,11 +33,23 @@ class WagesTestGameInitalizer extends GameInitializer {
         unset($oTables[$i]);
         $this->normalCase($case2Table);
 
-        //-- Case 3 Barman (2 case Playable & Not)
+        //-- Case 3&4 Barman (2 case Playable & Not)
         $i = random_int(0, count($oTables) - 1);
         $case3Table = $oTables[array_keys($oTables)[$i]];
         unset($oTables[$i]);
         $this->limitedCase($case3Table);
+
+        //-- Case 5 Searcher & wage > 2 (not Playable)
+        $i = random_int(0, count($oTables) - 1);
+        $case5Table = $oTables[array_keys($oTables)[$i]];
+        unset($oTables[$i]);
+        $this->searcherCase($case5Table);
+        
+        //-- Case 6 Writer & wage > 1 ( Playable)
+        $i = random_int(0, count($oTables) - 1);
+        $case6Table = $oTables[array_keys($oTables)[$i]];
+        unset($oTables[$i]);
+        $this->writerCase($case6Table);
 
         return $case1Table->getId();
     }
@@ -83,12 +95,56 @@ class WagesTestGameInitalizer extends GameInitializer {
         $forcedWage = new WageLevel2();
         $forcedWage->setLocation(CardLocation::PLAYER_HAND)
                 ->setLocationArg($table->getId());
-
         $this->cardManager->add($forcedWage);
+
         $forcedWage2 = new WageLevel1();
         $forcedWage2->setLocation(CardLocation::PLAYER_HAND)
                 ->setLocationArg($table->getId());
+        $this->cardManager->add($forcedWage2);
+    }
 
+    private function searcherCase(PlayerTable $table) {
+        $job = $this->cardManager->findBy(
+                ["type" => CardType::JOB_RESEARCHER], 1
+        );
+        $this->cardManager->playCard($table->getPlayer(), $job);
+        $table->addCard($job);
+        $this->playerTableManager->updateTable($table);
+
+        $forcedWage = new WageLevel2();
+        $forcedWage->setLocation(CardLocation::PLAYER_HAND)
+                ->setLocationArg($table->getId());
+        $this->cardManager->add($forcedWage);
+
+        $forcedWage2 = new WageLevel4();
+        $forcedWage2->setLocation(CardLocation::PLAYER_HAND)
+                ->setLocationArg($table->getId());
+        $this->cardManager->add($forcedWage2);
+    }
+    
+    private function writerCase(PlayerTable $table) {
+        $job = $this->cardManager->findBy(
+                ["type" => CardType::JOB_WRITER], 1
+        );
+        $this->cardManager->playCard($table->getPlayer(), $job);
+        $table->addCard($job);
+        
+        $grandPrix = $this->cardManager->findBy(
+                ["type" => CardType::REWARD_NATIONAL_MEDAL], 1
+        );
+        $this->cardManager->playCard($table->getPlayer(), $job);
+        $table->addCard($job);
+        
+        $this->playerTableManager->updateTable($table);
+
+        $forcedWage = new WageLevel2();
+        $forcedWage->setLocation(CardLocation::PLAYER_HAND)
+                ->setLocationArg($table->getId());
+        $this->cardManager->add($forcedWage);
+
+        $forcedWage2 = new WageLevel4();
+        $forcedWage2->setLocation(CardLocation::PLAYER_HAND)
+                ->setLocationArg($table->getId());
         $this->cardManager->add($forcedWage2);
     }
 
