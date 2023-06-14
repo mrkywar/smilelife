10a0d4d286ce4031a2340cf95e75378c34a78459
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-06-14 22:31:47 +0200

    [sc-568] advanced test case for studies

--- modules/php/Game/Table/PlayerTable.php
+++ modules/php/Game/Table/PlayerTable.php
@@ -20,6 +20,9 @@ use SmileLife\Card\Category\Special\JobBoost;
 use SmileLife\Card\Category\Special\Special;
 use SmileLife\Card\Category\Studies\Studies;
 use SmileLife\Card\Category\Wage\Wage;
+use SmileLife\Card\Criterion\JobCriterion\JobEffectCriteria;
+use SmileLife\Card\Effect\CardEffectInterface;
+use SmileLife\Card\Effect\Category\LimitlessStudiesEffect;
 
 /**
  * Description of Game
@@ -300,8 +303,9 @@ class PlayerTable extends Model {
     }
 
     public function addStudy(Studies $card) {
-        if ($card->getIsFlipped()) {
-            array_unshift($this->studiesIds, $card);
+        $criteria = new JobEffectCriteria($this, LimitlessStudiesEffect::class);
+        if ($criteria->isValided()) {
+            array_unshift($this->studiesIds, $card->getId());
         } else {
             $this->studiesIds[] = $card->getId();
         }
--- smilelife.game.php
+++ smilelife.game.php
@@ -129,7 +129,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new SmileLife\Game\Initializer\Test\AdulteryTestsInitializer();
+        $this->gameInitializer = new SmileLife\Game\Initializer\Test\StudieTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
