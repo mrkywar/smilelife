47eb90c0048db0115cbf55c4eedb2a20a7f1d2cb
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-06-19 21:39:40 +0200

    [sc-87] Notifiy discard Studie

--- modules/php/Game/Card/Consequence/Category/Generic/DiscardConsequence.php
+++ modules/php/Game/Card/Consequence/Category/Generic/DiscardConsequence.php
@@ -2,10 +2,12 @@
 
 namespace SmileLife\Card\Consequence\Category\Generic;
 
+use Core\Notification\Notification;
 use Core\Requester\Response\Response;
 use SmileLife\Card\Card;
 use SmileLife\Card\CardManager;
 use SmileLife\Card\Consequence\Consequence;
+use SmileLife\Card\Core\CardDecorator;
 use SmileLife\Table\PlayerTable;
 use SmileLife\Table\PlayerTableManager;
 
@@ -33,25 +35,47 @@ class DiscardConsequence extends Consequence {
      * @var CardManager
      */
     protected $cardManager;
-    
+
     /**
      * 
      * @var PlayerTableManager
      */
     protected $tableManager;
 
-    
-    
+    /**
+     * 
+     * @var CardDecorator
+     */
+    private $cardDecorator;
 
     public function __construct(Card $card, PlayerTable $table) {
         $this->cardManager = new CardManager();
         $this->tableManager = new PlayerTableManager();
+        $this->cardDecorator = new CardDecorator();
+
         $this->table = $table;
         $this->card = $card;
     }
 
     public function execute(Response &$response) {
-        $this->cardManager->discardCard($this->card, $this->table->getPlayer());
+        $player = $this->table->getPlayer();
+        $this->cardManager->discardCard($this->card, $player);
+
+        $notification = new Notification();
+
+        $discardedCards = $this->cardManager->getAllCardsInDiscard();
+
+        $notification->setType("discardNotification")
+                ->setText(clienttranslate('${player_name} discard ${cardName}'))
+                ->add('player_name', $player->getName())
+                ->add('playerId', $player->getId())
+                ->add('card', $this->cardDecorator->decorate($this->card))
+                ->add('cardName', (string) $this->card)
+                ->add('discard', $this->cardDecorator->decorate($discardedCards));
+        ;
+
+        $response->addNotification($notification);
+
         return $this;
     }
 
