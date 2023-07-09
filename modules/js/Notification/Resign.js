b0929d4e4354c68fcfe302894a412ff176bf9ea5
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-07-09 18:07:49 +0200

    [sc-616] Offiside complete

--- modules/js/Notification/Resign.js
+++ modules/js/Notification/Resign.js
@@ -26,20 +26,17 @@ define([
                     this.discardCounter.setValue(this.discardCounter.getValue() + 1);
                     this.boardCounter[notif.args.playerId][notif.args.card.pile].setValue(this.boardCounter[notif.args.playerId][notif.args.card.pile].getValue() - 1);
                 },
-                
-                notif_offsideNotification: function (notif) {
-                    this.debug("offside", notif.args);
-//                    var card = notif.args.card;
-//                    this.displayCard(card, "pile_offside", "pile_" + card.pile + "_" + notif.args.playerId);
 
-//                    if (parseInt(notif.args.playerId) === this.player_id) {
-//                        this.myTable = notif.args.table;
-//                    } else {
-//                        this.otherTabes[notif.args.playerId] = notif.args.table;
-//                    }
-//
-//                    this.offsideCounter.setValue(this.offsideCounter.getValue() + 1);
-//                    this.boardCounter[notif.args.playerId][notif.args.card.pile].setValue(this.boardCounter[notif.args.playerId][notif.args.card.pile].getValue() - 1);
+                notif_offsideNotification: function (notif) {
+//                    this.debug("offside", notif.args);
+
+                    for (var cardIndex in notif.args.cards) {
+                        var card = notif.args.cards[cardIndex];
+                        this.debug("CARD", card);
+                        this.displayCard(card, "pile_offside", "pile_" + card.pile + "_" + notif.args.playerId);
+                        this.offsideCounter.setValue(this.offsideCounter.getValue() + 1);
+                        this.boardCounter[notif.args.playerId][card.pile].setValue(this.boardCounter[notif.args.playerId][card.pile].getValue() - 1);
+                    }
                 }
             }
     );
