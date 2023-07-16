366a45974a472c14ce01a492972d9f7f3f597f6c
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-07-16 10:55:20 +0200

    [sc-636] Prepare notification

--- modules/php/Game/Card/Consequence/Category/Special/ShuffleHandAgainConsequence.php
+++ modules/php/Game/Card/Consequence/Category/Special/ShuffleHandAgainConsequence.php
@@ -2,11 +2,16 @@
 
 namespace SmileLife\Card\Consequence\Category\Special;
 
+use Core\Notification\Notification;
 use Core\Requester\Response\Response;
+use SmileLife\Card\Card;
 use SmileLife\Card\CardManager;
 use SmileLife\Card\Consequence\Consequence;
 use SmileLife\Card\Core\CardDecorator;
 use SmileLife\Card\Core\CardLocation;
+use SmileLife\PlayerAttributes\PlayerAttributes;
+use SmileLife\PlayerAttributes\PlayerAttributesManager;
+use SmileLife\Table\PlayerTable;
 use SmileLife\Table\PlayerTableManager;
 
 /**
@@ -33,27 +38,62 @@ class ShuffleHandAgainConsequence extends Consequence {
      * @var CardDecorator
      */
     protected $cardDecorator;
+    
+    /**
+     * 
+     * @var PlayerAttributesManager
+     */
+    private $playerAttributeManager;
 
     public function __construct() {
         $this->tableManager = new PlayerTableManager();
         $this->cardManager = new CardManager();
         $this->cardDecorator = new CardDecorator();
+        $this->playerAttributeManager = new PlayerAttributesManager();
     }
 
-    
     public function execute(Response &$response) {
         $cards = $this->cardManager->findBy([
-                'location' => CardLocation::PLAYER_HAND
+            'location' => CardLocation::PLAYER_HAND
         ]);
-        
-        
-        var_dump($cards);
+
         shuffle($cards);
-        var_dump($cards);
-        die;
-//        $tables = $this->tableManager->findBy();
-//        foreach ($tables as $table) {
-//            $this->offsideTableChilds($table, $response);
-//        }
+
+        $tables = $this->tableManager->findBy();
+        foreach ($tables as $table) {
+            $attribute = $this->playerAttributeManager->findBy([
+                "id" => $table->getPlayer()->getId()
+            ]);
+            $newCards = $this->redistributeCards($cards, $table, $attribute);
+
+            $notification = new Notification();
+            $notification->setType("handChangedNotification")
+                    ->setPublic(false)
+                    ->setText(clienttranslate('Your hand was changed by an external event'))
+                    ->add("card", $this->cardDecorator->decorate($newCards));
+            
+            $response->addNotification($notification);
+        }
     }
+
+    private function redistributeCards(array &$cards, PlayerTable $table, PlayerAttributes $attribute) {
+        $givedCard = [];
+        $player = $table->getPlayer();
+
+        for ($i = 1; $i <= $attribute->getMaxCards(); $i++) {
+            /**
+             * @var Card
+             */
+            $card = array_shift($cards);
+
+            $card->setLocation(CardLocation::PLAYER_HAND)
+                    ->setLocationArg($player->getId());
+            $givedCard[] = $card;
+        }
+
+
+        $this->cardManager->update($givedCard);
+        return $givedCard;
+    }
+
 }
