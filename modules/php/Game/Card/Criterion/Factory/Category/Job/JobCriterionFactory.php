56902d5ce266e56a4d179b0cf6f94e19bd3f7dbb
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-07-22 21:10:57 +0200

    [sc-663] Fixed Job Playable by jobBoost

--- modules/php/Game/Card/Criterion/Factory/Category/Job/JobCriterionFactory.php
+++ modules/php/Game/Card/Criterion/Factory/Category/Job/JobCriterionFactory.php
@@ -34,7 +34,7 @@ class JobCriterionFactory extends CardCriterionFactory {
     public function create(PlayerTable $table, Card $card, PlayerTable $opponentTable = null, array $complementaryCards = null): CriterionInterface {
         $pistonCriterion = new HaveJobBoostReadyCriterion($table);
         $jobBoost = $table->getJobBoost();
-        if(null !== $jobBoost){
+        if (null !== $jobBoost) {
             $pistonCriterion->addConsequence(new JobBoostUsedConsequence($jobBoost, $card, $table));
         }
 
@@ -44,14 +44,23 @@ class JobCriterionFactory extends CardCriterionFactory {
         $jobStudieCriterion = new JobStudiesCriterion($table, $card);
         $jobStudieCriterion->setErrorMessage(clienttranslate('You do not have enough study points to perform this job'));
 
+        //-- V1
+//        $criteria = new CriterionGroup([
+//                $pistonCriterion,
+//                new CriterionGroup([
+//                    $noJobCriterion,
+//                    $jobStudieCriterion
+//                ], CriterionGroup::AND_OPERATOR)
+//            ], CriterionGroup::OR_OPERATOR);
+
         $criteria = new CriterionGroup([
-                $pistonCriterion,
-                new CriterionGroup([
-                    $noJobCriterion,
+            $noJobCriterion,
+            new CriterionGroup([
+                    $pistonCriterion,
                     $jobStudieCriterion
                 ], CriterionGroup::AND_OPERATOR)
-            ], CriterionGroup::OR_OPERATOR);
-        
+            ], CriterionGroup::AND_OPERATOR);
+
         $criteria->addConsequence(new GenericCardPlayedConsequence($card, $table));
 
         return $criteria;
--- modules/php/Game/Game/Initializer/Test/ClassicJobsTestInitializer.php
+++ modules/php/Game/Game/Initializer/Test/ClassicJobsTestInitializer.php
@@ -5,7 +5,7 @@ namespace SmileLife\Game\Initializer\Test;
 use SmileLife\Card\Category\Job\Interim\Stripteaser;
 use SmileLife\Card\Category\Job\Job;
 use SmileLife\Card\Category\Job\Job\Astronaut;
-use SmileLife\Card\Category\Job\Job\Writer;
+use SmileLife\Card\Category\Job\Job\Designer;
 use SmileLife\Card\Category\Special\JobBoost;
 use SmileLife\Card\Category\Studies\StudiesLevel1;
 use SmileLife\Card\Core\CardLocation;
@@ -23,11 +23,11 @@ class ClassicJobsTestInitializer extends TestGameInitializer {
 
         $oTables = $this->playerTableManager->findBy();
 
-        $job = new Writer();
+        $job = new Designer();
 
         $forcedCards = [];
         foreach ($oTables as $oTable) {
-            $card = new Writer();
+            $card = new Designer();
             $card->setLocation(CardLocation::PLAYER_HAND)
                     ->setLocationArg($oTable->getId());
             $forcedCards[] = $card;
