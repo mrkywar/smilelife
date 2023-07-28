d457b5e2a9f10cd41d7203be2b5087b6375aa4ab
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-07-28 20:16:11 +0200

    [sc-671] Test Researcher Init

--- smilelife.game.php
+++ smilelife.game.php
@@ -137,7 +137,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new SmileLife\Game\Initializer\Test\GuruAndBanditJobsTestInitializer();
+        $this->gameInitializer = new SmileLife\Game\Initializer\Test\ResearcherJobTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
