97e5b83904a45b4a1b3c3b843feb821ccbb573ba
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-07-28 16:07:32 +0200

    [sc-672] Draw Consequence

---
+++ modules/php/Game/Card/Consequence/Category/Generic/PlayerDrawConsequence.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SmileLife\Card\Consequence\Category\Generic;
+
+use Core\Requester\Response\Response;
+use SmileLife\Card\CardManager;
+use SmileLife\Card\Consequence\PlayerTableConsequence;
+use SmileLife\Card\Core\CardLocation;
+use SmileLife\Table\PlayerTable;
+
+/**
+ * Description of PlayerDrawConsequence
+ *
+ * @author Mr_Kywar mr_kywar@gmail.com
+ */
+class PlayerDrawConsequence extends PlayerTableConsequence {
+
+    /**
+     * 
+     * @var CardManager
+     */
+    protected $cardManager;
+
+    /**
+     * 
+     * @var CardDecorator
+     */
+    private $cardDecorator;
+
+    public function __construct(PlayerTable $table) {
+        parent::__construct($table);
+
+        $this->cardManager = new CardManager();
+        $this->cardDecorator = new CardDecorator();
+    }
+
+    public function execute(Response &$response) {
+        $player = $this->table->getPlayer();
+
+        $card = $this->cardManager->drawCard();
+
+        $card->setLocation(CardLocation::PLAYER_HAND)
+                ->setLocationArg($player->getId());
+
+        $this->cardManager->moveCard($card);
+
+        $notification = new Notification();
+
+        $notification->setType("drawNotification")
+                ->setText(clienttranslate('${player_name} draw a card from the deck by external event'))
+                ->add('player_name', $player->getName())
+                ->add('playerId', $player->getId())
+                ->add('card', $this->cardDecorator->decorate($card))
+        ;
+
+        $response->addNotification($notification);
+
+        return $response;
+    }
+
+}
