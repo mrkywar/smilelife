e59502b03b0f160301e857cdf20658d3a4c9093a
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-09-29 21:04:18 +0200

    [sc-723]

---
+++ modules/php/Game/Game/Initializer/Test/TrocSpecialTestInitializer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace SmileLife\Game\Initializer\Test;
+
+use SmileLife\Card\Category\Special\Troc;
+use SmileLife\Card\Core\CardLocation;
+
+/**
+ * Description of TrocSpecialTestInitializer
+ *
+ * @author Mr_Kywar mr_kywar@gmail.com
+ */
+class TrocSpecialTestInitializer extends TestGameInitializer {
+
+    public function init($players, $options = []) {
+        parent::init($players, $options);
+
+        $oTables = $this->playerTableManager->findBy();
+
+        $forcedCards = [];
+        foreach ($oTables as $oTable) {
+            $card = new Troc();
+            $card->setLocation(CardLocation::PLAYER_HAND)
+                    ->setLocationArg($oTable->getId());
+            $forcedCards[] = $card;
+        }
+        $this->cardManager->add($forcedCards);
+
+        reset($oTables);
+
+        $i = random_int(0, count($oTables) - 1);
+        $case1Table = $oTables[array_keys($oTables)[$i]];
+
+        return $case1Table->getId();
+    }
+
+}
