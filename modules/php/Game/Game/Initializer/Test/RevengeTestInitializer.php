bcf3cb8d830174293404fea09263bc77776554c1
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-09-30 09:30:53 +0200

    [sc-699] Test init

---
+++ modules/php/Game/Game/Initializer/Test/RevengeTestInitializer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SmileLife\Game\Initializer\Test;
+
+use SmileLife\Card\Category\Special\Revenge;
+use SmileLife\Card\Core\CardLocation;
+
+/**
+ * Description of RevengeTestInitializer
+ *
+ * @author Mr_Kywar mr_kywar@gmail.com
+ */
+class RevengeTestInitializer extends TestGameInitializer {
+
+    public function init($players, $options = []) {
+        parent::init($players, $options);
+
+        $oTables = $this->playerTableManager->findBy();
+
+        $forcedCards = [];
+        
+        foreach ($oTables as $oTable) {
+            $card = new Revenge();
+            $card->setLocation(CardLocation::PLAYER_HAND)
+                    ->setLocationArg($oTable->getId());
+            $forcedCards[] = $card;
+        }
+        $this->cardManager->add($forcedCards);
+
+        reset($oTables);
+
+        $i = random_int(0, count($oTables) - 1);
+        $case1Table = $oTables[array_keys($oTables)[$i]];
+
+        return $case1Table->getId();
+    }
+
+}
--- smilelife.game.php
+++ smilelife.game.php
@@ -138,7 +138,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new SmileLife\Game\Initializer\Test\TrocSpecialTestInitializer();
+        $this->gameInitializer = new SmileLife\Game\Initializer\Test\RevengeTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
