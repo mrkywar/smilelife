051a0d0f6ebfa6d3fe1b605ddfd2a7fd2b326c50
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-10-16 21:53:30 +0200

    [sc-736] Prepare Specific initializer

---
+++ modules/php/Game/Game/Initializer/Test/JailDrawTestInitializer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SmileLife\Game\Initializer\Test;
+
+use SmileLife\Card\Category\Attack\Jail;
+use SmileLife\Card\Category\Job\Job\Bandit;
+use SmileLife\Card\Core\CardLocation;
+
+/**
+ * Description of JailDrawTestInitializer
+ * sc-736 : Jail draw and playable (without bandit)
+ * @author Mr_Kywar mr_kywar@gmail.com
+ */
+class JailDrawTestInitializer extends TestGameInitializer {
+
+    public function init($players, $options = []) {
+        parent::init($players, $options);
+
+        $oTables = $this->playerTableManager->findBy();
+        $forcedCards = [];
+
+        $jailInDraw = new Jail();
+        $jailInDraw->setLocation(CardLocation::DECK)
+                ->setLocationArg(0); // First place
+        $forcedCards[] = $jailInDraw;
+        $this->cardManager->add($forcedCards);
+
+        foreach ($oTables as $oTable) {
+            $forcedCards = [];
+            $card = new Bandit();
+            $card->setLocation(CardLocation::PLAYER_HAND)
+                    ->setLocationArg($oTable->getId());
+            $forcedCards[] = $card;
+
+            $this->cardManager->add($forcedCards);
+            $this->playWaitingCards($oTable);
+        }
+
+
+        $i = random_int(0, count($oTables) - 1);
+        $case1Table = $oTables[array_keys($oTables)[$i]];
+        return $case1Table->getId();
+    }
+
+}
