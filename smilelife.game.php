c986dd6682ce49e4ad64805a5060f19a65faaa61
mykywar <88911503+mrkywar@users.noreply.github.com>
2023-10-17 16:26:31 +0200

    [sc-806] Prepare Tests cases

--- smilelife.game.php
+++ smilelife.game.php
@@ -139,7 +139,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\RevengeFromDiscardTestInitializer();
+        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\GradeRepetitionFromDiscardTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
