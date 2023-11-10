b2074fa9db75970053a255a5f98b5e88fb788eaa
mrkywar <88911503+mrkywar@users.noreply.github.com>
2023-11-10 19:59:12 +0100

    [sc-820] Rainbow Prepare testing case

--- smilelife.game.php
+++ smilelife.game.php
@@ -140,7 +140,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\AstronautJobTestInitializerV2();
+        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\RainbowTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
