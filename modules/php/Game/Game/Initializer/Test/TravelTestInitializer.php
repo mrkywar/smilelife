daa6f2edda9e2fe311352b8702341670d5ff1b82
mrkywar <88911503+mrkywar@users.noreply.github.com>
2024-01-09 21:01:11 +0100

    [sc-830] Test initilizer

---
+++ modules/php/Game/Game/Initializer/Test/TravelTestInitializer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SmileLife\Game\Initializer\Test;
+
+use SmileLife\Card\CardType;
+use SmileLife\Card\Category\Job\Interim\Barman;
+use SmileLife\Card\Category\Special\Casino;
+use SmileLife\Card\Category\Wage\WageLevel1;
+use SmileLife\Card\Category\Wage\WageLevel2;
+use SmileLife\Card\Core\CardLocation;
+
+/**
+ * Description of TravelTestInitializer
+ *
+ * @author Mr_Kywar mr_kywar@gmail.com
+ */
+class TravelTestInitializer extends TestGameInitializer {
+
+    public function init($players, $options = []) {
+        parent::init($players, $options);
+
+        $oTables = $this->playerTableManager->findBy();
+
+        $travels = [
+            new \SmileLife\Card\Category\Acquisition\Travel\Cairo(),
+            new \SmileLife\Card\Category\Acquisition\Travel\London(),
+            new \SmileLife\Card\Category\Acquisition\Travel\NewYork(),
+            new \SmileLife\Card\Category\Acquisition\Travel\RioDeJaneiro(),
+            new \SmileLife\Card\Category\Acquisition\Travel\Sydney()
+        ];
+
+        foreach ($oTables as $oTable) {
+            $card = array_shift($travels);
+            if (null !== $card) {
+                $card->setLocation(CardLocation::PLAYER_HAND)
+                        ->setLocationArg($oTable->getId());
+
+                $this->cardManager->add($card);
+            }
+        }
+
+        $i = random_int(0, count($oTables) - 1);
+        $table =  $oTables[array_keys($oTables)[$i]];
+        
+        return $table->getId();
+    }
+}
--- smilelife.game.php
+++ smilelife.game.php
@@ -148,7 +148,7 @@ class SmileLife extends Table {
         self::$instance = $this;
 
 //        $this->gameInitializer = new GameInitializer();
-        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\CasinoTestInitializer();
+        $this->gameInitializer = new \SmileLife\Game\Initializer\Test\TravelTestInitializer();
         $this->progressionRetriver = new GameProgressionRetriver();
         $this->dataRetriver = new DataRetriver();
 
