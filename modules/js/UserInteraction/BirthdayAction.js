0d5da5f97273cf5328fc96bbc0a4c1712b1ca8e4
mrkywar <88911503+mrkywar@users.noreply.github.com>
2024-04-03 22:21:30 +0200

    [sc-912] Create Give Wage Modale

---
+++ modules/js/UserInteraction/BirthdayAction.js
@@ -0,0 +1,25 @@
+define([
+    "dojo",
+    "dojo/_base/declare",
+], function (dojo, declare) {
+    return declare(
+            "smilelife.ui.birthdayAction",
+            [],
+            {
+                constructor: function () {
+                    
+                },
+                
+                addBirthdayInteraction: function(){
+                    var modalTitle = _('Choose the wage to offer');
+                    var id = this.openModal(modalTitle, MODAL_TYPE_BIRTHDAY, null, this.getUsableWages());
+                },
+                
+                onWageBirthdayClick:function(playedCard, additionalCard, id){
+                    this.debug(playedCard, additionalCard, id);
+                }
+            }
+
+    );
+});
+
