2e2600f91cde622f21a2f5e8aec0b4ba79ddff82
mrkywar <88911503+mrkywar@users.noreply.github.com>
2024-04-21 16:52:21 +0200

    [sc-935] Fix update counter

--- modules/js/Notification/Play.js
+++ modules/js/Notification/Play.js
@@ -39,15 +39,11 @@ define([
                     //-- UPDATE Counters
                     if ("discard" === notif.args.from) {
                         this.discardCounter.setValue(this.discardCounter.getValue() - 1);
-                    } else {
+                    } else if (card.locationArg !== notif.args.targetId) {
                         this.boardCounter[card.locationArg][card.pile].setValue(this.boardCounter[card.locationArg][card.pile].getValue() - 1);
                     }
-//                    if ("discard" === card.location) {
-//                        this.discardCounter.setValue(this.discardCounter.getValue() - 1);
-//                    } else if (card.locationArg !== notif.args.targetId) {
-//                        this.boardCounter[card.locationArg][card.pile].setValue(this.boardCounter[card.locationArg][card.pile].getValue() - 1);
-//                    }
                     this.boardCounter[notif.args.targetId][card.pile].setValue(this.boardCounter[notif.args.targetId][card.pile].getValue() + 1);
+
 
                     //-- Job redisplay (to keep on the top)
                     if ("job" === card.pile && "studies" === card.categorie) {
