2716bedf14731ab0a1d113c2306d91bf4b4d360b
mrkywar <88911503+mrkywar@users.noreply.github.com>
2024-05-11 20:47:49 +0200

    [sc-942] Forced End removed

--- modules/php/Game/Game/Traits/NextPlayerTrait.php
+++ modules/php/Game/Game/Traits/NextPlayerTrait.php
@@ -36,8 +36,7 @@ trait NextPlayerTrait {
         $cardDecorator = new CardDecorator();
         $tableDecorator = new PlayerTableDecorator();
 
-        //--- TODO remove force end
-        if (1 == 1 || 0 === count($deckCard)) {
+        if ( 0 === count($deckCard)) {
             $scoreDecorator = new ScoreDecorator();
             $playerTables = $this->tableManager->findBy();
 
